5a5ba984a8a15451579edc1dd4b7c41916f08232
Abdelrahman Sherif <161166808+Abdelrahman2094@users.noreply.github.com>
2025-05-12 02:36:37 +0300

    Delete routes directory

--- routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/contactRoutes.js
-const express = require('express');
-const router = express.Router();
-const { createContact } = require('../controllers/contactController.js');
-
-
-router.post('/', createContact);
-
-module.exports = router;
